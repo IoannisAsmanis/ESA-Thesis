9caedb5796579c1722695c4dd205f654c14cb797
Matteo De Benedetti <41896432+MatteoDeBenedetti@users.noreply.github.com>
2019-09-23 14:19:55 +0200

    Update quaternion2heading.m

--- VO_logs_visualization/quaternion2heading.m
+++ VO_logs_visualization/quaternion2heading.m
@@ -1,15 +1,34 @@
 function [hdg] = quaternion2heading(q)
 %UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%   INPUT: q: quaternion expressed as wxyz (aka: abcd or re,im)
 
-q(2) = 0;
-q(4) = 0;
-mag = sqrt(q(1)*q(1)+q(3)*q(3));
+% if an invalid quaternion is passed
+if (numel(q) < 4)
+    disp("quaternion2heading(q) usage: must pass a 4 elements vector or n-by-4 array ")
 
-q(1) = q(1)/mag;
-q(3) = q(3)/mag;
+% if a single quaternion is given
+elseif (numel(q) == 4)
+    q(2) = 0;
+    q(4) = 0;
+    mag = sqrt(q(1)*q(1)+q(3)*q(3));
+    
+    q(1) = q(1)/mag;
+    q(3) = q(3)/mag;
+    
+    hdg = 2*acos(q(1));
 
-hdg = 2*acos(q(1));
+% if an array of quaternions is passed
+else 
+    q(:,2) = 0;
+    q(:,4) = 0;
+    mag = sqrt(q(:,1).*q(:,1)+q(:,3).*q(:,3));
+    
+    q(:,1) = q(:,1)./mag;
+    q(:,3) = q(:,3)./mag;
+    
+    hdg = 2*acos(q(:,1));
+    
+end
 
 end
 
