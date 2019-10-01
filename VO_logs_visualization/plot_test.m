6741f733aab92e86f7207386038fdd974d569d42
Matteo De Benedetti <41896432+MatteoDeBenedetti@users.noreply.github.com>
2019-10-01 16:36:01 +0200

    fixed control file not present error

--- VO_logs_visualization/plot_test.m
+++ VO_logs_visualization/plot_test.m
@@ -4,14 +4,14 @@ close all
 
 
 % set log path (uncomment one)
-path = 'logs/20190912-1551';
-path = 'logs/20190913-1250';
+% path = 'logs/20190912-1551';
+% path = 'logs/20190913-1250';
 % path = 'logs/20190913-1239.1'; %nope
 % path = 'logs/20190913-1314'; %not too far from 2%
 % path = 'logs/20190913-1436';
 % path = 'logs/20190913-1646'; %going back
 % path = 'logs/20190913-1707'; % move along x and then along y
-path = 'logs/20190916-1428'; % move along x and then along y ---
+% path = 'logs/20190916-1428'; % move along x and then along y ---
 % path = 'logs/20190916-1530';
 % path = 'logs/20190916-1716';
 % path = 'logs/20190916-1811'; %control
@@ -47,29 +47,41 @@ path = 'logs/20190916-1428'; % move along x and then along y ---
 % path = 'logs/20190927-1330'; %test with 20190919-1255 and new orient old pos
 % path = 'logs/20190930-1700'; %test with 20190919-1255 and new orient old pos
 % new vo params
-% path = 'logs/20190930-1700'; %test with 20190919-1255 and new orient old pos
-path = 'logs/20191001-1302'; %test with 20190919-1255 and new orient old pos
+% path = 'logs/20190930-1700'; 
+% path = 'logs/20191001-1302'; 
+% path = 'logs/20191001-1417'; 
+% path = 'logs/20191001-1422'; 
+path = 'logs/20191001-1554'; 
 
+CONTROL_FILE = false;
 
 % Read odometry file
 [t, x, y, z, b, c, d, a]=textread(horzcat(path,'/odom_world.txt'), ...
     '%d%f%f%f%*f%*f%*f%*f%*f%*f%*f%*f%*f%f%f%f%f%*[^\n]', 'headerlines', 2, 'delimiter', '\t');
 odom_pose = [t, x, y, z, a, b, c, d];
 
+% %%% sneaky trick do not uncomment
+% for i=1:size(odom_pose,1)
+%     odom_pose(i,2:4) = odom_pose(i,2:4)-(gt_pose(1,2:4)-odom_pose(1,2:4));
+% end
+% %%%
+
 % Read difference odometry/vicon file
 [t, x, y, z, b, c, d, a]=textread(horzcat(path,'/diff_pose.txt'), ...
     '%d%f%f%f%*f%*f%*f%*f%*f%*f%*f%*f%*f%f%f%f%f%*[^\n]', 'headerlines', 2, 'delimiter', '\t');
 diff_pose = [t, x, y, z, a, b, c, d];
 
-% % Read control file
-% [ tr, rot, hdg]=textread(horzcat(path,'/control.txt'), ...
-%     '%f%f%f%*[^\n]', 'headerlines', 2, 'delimiter', '\t');
-% 
-% % Read control time file
-% t=textread(horzcat(path,'/control_time.txt'), ...
-%     '%d', 'headerlines', 2);
-% % t(end+1) = t(end);
-% control = [t, tr, rot];
+if(CONTROL_FILE)
+    % Read control file
+    [ tr, rot, hdg]=textread(horzcat(path,'/control.txt'), ...
+        '%f%f%f%*[^\n]', 'headerlines', 2, 'delimiter', '\t');
+    
+    % Read control time file
+    t=textread(horzcat(path,'/control_time.txt'), ...
+        '%d', 'headerlines', 2);
+    % t(end+1) = t(end);
+    control = [t, tr, rot];
+end
 
 % read real gt file
 [t, x, y, z]=textread(horzcat(path,'/gt_pose.txt'), ...
@@ -94,20 +106,22 @@ gt_pose(:,9) = hdg;
 
 %% PROCESS DATA
 
-%%%
+%%% sneaky trick 2, do not uncomment
 % for i = 2:size(odom_pose,1)
 %     odom_pose(i,2) = odom_pose(i,2) +1*(odom_pose(i,2)-odom_pose(i-1,2));
 % end
-% diff_pose(:,2:4) = gt_pose(:,2:4) - odom_pose(:,2:4);
 %%%
 
 % convert odom quaternion to odom heading
-hdg = quaternion2heading(odom_pose(:,5:8));
+% hdg = quaternion2heading(odom_pose(:,5:8));
 % odom_pose(:,9) = hdg;
 
 % convert gt quaternion to gt heading
-hdg = quaternion2heading(gt_pose(:,5:8));
+% hdg = quaternion2heading(gt_pose(:,5:8));
 % gt_pose(:,9) = hdg;
+
+% Create diff pose (gt - odom = diff)
+diff_pose = [odom_pose(:,1) gt_pose(:,2:end) - odom_pose(:,2:end)];
 
 % convert diff quaternion to diff heading
 hdg = quaternion2heading(diff_pose(:,5:8));
@@ -120,13 +134,14 @@ diff_pose(:,9) = hdg;
 odom_pose(:,1) = (odom_pose(:,1) - odom_pose(1,1))/10^6;
 diff_pose(:,1) = (diff_pose(:,1) - diff_pose(1,1))/10^6;
 gt_pose(:,1) = (gt_pose(:,1) - gt_pose(1,1))/10^6;
-% control(:,1) = (control(:,1) - control(1,1))/10^6;
-% gt_pose2(:,1) = (gt_pose2(:,1) - gt_pose2(1,1))/10^6;
+if(CONTROL_FILE)
+    control(:,1) = (control(:,1) - control(1,1))/10^6;
+end
 
 % compute error norm
 diff_norm = zeros(size(diff_pose,1),1);
 for i = 1:size(diff_pose,1)
-    diff_norm(i) = norm(diff_pose(i,2:3));
+    diff_norm(i) = norm(diff_pose(i,2:4));
 end
 
 % travelled distance up to now
@@ -169,11 +184,13 @@ plot(diff_pose(:,1), diff_norm, 'b-', diff_pose(:,1), dist_accum.*0.02, 'r-');
 grid on, legend('xy error norm', '2% travelled distance');
 xlabel('time [s]'), ylabel('xy error [m]'), title('Visual Odometry Evaluation - xy error norm');
 
-% % x and y components error and control over time
-% figure(6);
-% plot(diff_pose(:,1), diff_pose(:,2), 'b-', diff_pose(:,1), diff_pose(:,3), 'r-', control(:,1), control(:,2));
-% grid on, legend('x error', 'y error', 'control');
-% xlabel('time [s]'), ylabel('xy error [m] and control [m/s]'), title('Visual Odometry Evaluation - x y error components and control');
+% x and y components error and control over time
+if(CONTROL_FILE)
+    figure(6);
+    plot(diff_pose(:,1), diff_pose(:,2), 'b-', diff_pose(:,1), diff_pose(:,3), 'r-', control(:,1), control(:,2));
+    grid on, legend('x error', 'y error', 'control');
+    xlabel('time [s]'), ylabel('xy error [m] and control [m/s]'), title('Visual Odometry Evaluation - x y error components and control');
+end
 
 % x and y components error and x and y trajectory over time 
 figure(6);
@@ -184,12 +201,14 @@ subplot(2,1,2), plot(gt_pose(:,1), gt_pose(:,2), gt_pose(:,1), gt_pose(:,3));
 grid on, legend('GT x', 'GT y');
 xlabel('time [s]'), ylabel('xy error [m]');
 
-% % control over time
-% figure(7);
-% plot(control(:,1), control(:,2), control(:,1), control(:,3));
-% grid on, legend('translation', 'rotation');
-% xlabel('time [s]'), ylabel('command'), title('Visual Odometry Evaluation - translation and rotation commands');
-% 
+% control over time
+if(CONTROL_FILE)
+    figure(7);
+    plot(control(:,1), control(:,2), control(:,1), control(:,3));
+    grid on, legend('translation', 'rotation');
+    xlabel('time [s]'), ylabel('command'), title('Visual Odometry Evaluation - translation and rotation commands');
+end
+
 % % z over distance travelled
 % figure(8);
 % plot(dist_accum, gt_pose(:,4));
@@ -222,6 +241,16 @@ grid on, xlim([0 5]), ylim([2.5 7.5]); %legend('odom heading', 'GT heading');
 xlabel('position [m]'), ylabel('position [m]'), zlabel('time [s]');
 title('Visual Odometry Evaluation - xy trajectory over time');
 
-
+% % TBD
+% % xyz error norm over time vs 2% distance travelled
+% % xyz error norm over time vs 2% distance travelled ignoring the 1st estimate
+% figure(13);
+% subplot(2,1,1), plot(diff_pose(:,1), diff_norm, 'b-', diff_pose(:,1), dist_accum.*0.02, 'r-');
+%     grid on, legend('xy error norm', '2% travelled distance');
+%     xlabel('time [s]'), ylabel('xy error [m]'), title('Visual Odometry Evaluation - xy error norm');
+% subplot(2,1,2), plot(diff_pose(:,1), diff_norm-diff_norm(1), 'b-', diff_pose(:,1), dist_accum.*0.02-dist_accum(1), 'r-');
+%     grid on, legend('xy error norm', '2% travelled distance');
+%     xlabel('time [s]'), ylabel('xy error [m]'), title('Visual Odometry Evaluation - xy error norm');
+% 
 
 
