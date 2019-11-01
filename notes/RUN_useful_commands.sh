55c583ae08624d1596fa8895626ae9a8a6cef3dd
Matteo De Benedetti <levin.gerdes@esa.int>
2019-11-01 16:17:52 +0100

    updated to also export spartan logs

--- notes/RUN_useful_commands.sh
+++ notes/RUN_useful_commands.sh
@@ -4,3 +4,5 @@ pocolog unit_visual_odometry.0.log -s /viso2_evaluation.diff_pose >> diff_pose.t
 pocolog unit_visual_odometry.0.log -s /viso2_evaluation.odometry_heading >> odometry_heading.txt
 pocolog unit_visual_odometry.0.log -s /viso2_evaluation.ground_truth_heading >> gt_heading.txt
 pocolog unit_visual_odometry.0.log -s /viso2_evaluation.ground_truth_pose >> gt_pose.txt  
+pocolog unit_visual_odometry.0.log -s /viso2_evaluation.perc_error >> perc_error.txt  
+pocolog unit_visual_odometry.0.log -s /viso2_evaluation.travelled_distance >> travelled_distance.txt  
