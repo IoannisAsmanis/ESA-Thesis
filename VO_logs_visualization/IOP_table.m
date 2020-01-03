5e2b840303ff269643525d41c4ea7caac423f8e2
Matteo De Benedetti <41896432+MatteoDeBenedetti@users.noreply.github.com>
2020-01-03 13:32:45 +0100

    Created IOP calculator in Matlab

---
+++ VO_logs_visualization/IOP_table.m
@@ -0,0 +1,56 @@
+%%
+clear args 
+clear IFDs
+clear IOPs
+
+% args = cam, pitch, speed, period
+
+args = { 
+%     {'loccam', 30.4, 0.02, 15},
+    {'loccam', 30.4, 0.07, 4.2857},
+    {'navcam', 37.7, 0.07, 2},
+    {'navcam', 37.7, 0.07, 4.2857}};
+
+args = {
+    {'navcam', 20, 0.07, 4.2857},
+    {'navcam', 30, 0.07, 4.2857},
+    {'loccam', 30, 0.07, 4.2857},
+    {'navcam', 40, 0.07, 4.2857},
+    {'navcam', 50, 0.07, 4.2857}};
+%     {'navcam', 20, 0.07, 1},
+%     {'navcam', 30, 0.07, 1},
+%     {'navcam', 40, 0.07, 1},
+%     {'navcam', 50, 0.07, 1}};
+
+args = {
+    {'loccam', 30, 0.02, 0.5},
+    {'loccam', 30, 0.07, 0.428},
+    {'loccam', 30, 0.07, 1},
+    {'loccam', 30, 0.07, 1.42857},
+    {'loccam', 30, 0.07, 2.8571},
+    {'loccam', 30, 0.07, 4.2857},
+    {'loccam', 30, 0.07, 7.1428},
+    {'loccam', 30, 0.07, 10.7143},
+    {'loccam', 30, 0.07, 14.2857}};
+
+% args = { % cam, pitch, speed, period
+%     {'loccam', 30.4, 0.07, 4.2857},
+%     {'navcam', 30.4, 0.07, 12},
+%     {'loccam', 30.4, 0.07, 1.42875},
+%     {'navcam', 30.4, 0.07, 3.5},    
+%     {'loccam', 30.4, 0.07, 10.7143},
+%     {'navcam', 30.4, 0.07, 28}};
+
+
+for i = 1:size(args,1)
+    args{i}{5} = args{i}{3}*args{i}{4};
+    args{i}{6} = computeIOP(args{i}{1}, args{i}{2}, args{i}{3}, args{i}{4});
+    IFDs(i) = args{i}{5};
+    IOPs(i) = args{i}{6};
+end
+
+for i=1:size(args,1)
+    args{i}
+end
+IFDs
+IOPs
---
+++ functions/computeIOP.m
@@ -0,0 +1,35 @@
+function [IOP] = computeIOP(cam, pitch, vel, vo_period)
+%COMPUTEIOP Summary of this function goes here
+%   Detailed explanation goes here
+
+VFOV = deg2rad(49);
+HFOV = deg2rad(53);
+if strcmp(cam, 'loccam')
+    h = 0.24;
+    pitch = 30.4;
+elseif strcmp(cam, 'navcam')
+    h = 0.633;
+end
+pitch = deg2rad(pitch);
+    
+l1 = h/(cos(pi/2 - pitch - VFOV/2));
+l2 = h/(cos(pi/2 - pitch + VFOV/2));
+
+c = sin(VFOV/2 + pi/2)/sin(pi/2 - pitch)*(l2-l1);
+g = l1*sin(pi/2 - pitch - VFOV/2);
+
+a = 2*tan(HFOV/2)*c;
+b = 2*tan(HFOV/2)*(c + g);
+theta = atan( ((b - a)/2)/c );
+
+a_ov = a;
+c_ov = max(0, c - vel*vo_period);
+% l_ov = c_ov/sin(pi/2 - theta);
+b_ov = a_ov + 2*tan(theta)*c_ov;
+
+ov_area = (a_ov + b_ov)*c_ov/2;
+
+IOP = ov_area*100/((a + b)*c/2);
+
+end
+
